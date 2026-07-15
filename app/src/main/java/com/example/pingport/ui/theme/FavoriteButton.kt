c7007c15a0f28759ae0062600e6a559d27e604ac
jjjapanmarket <ttjump.jun.0404@gmail.com>
2026-07-15 20:51:07 +0900

    FavoriteButtonの箱を追加

--- app/src/main/java/com/example/pingport/ui/theme/FavoriteButton.kt
+++ app/src/main/java/com/example/pingport/ui/theme/FavoriteButton.kt
@@ -1,2 +1,11 @@
 package com.example.pingport.ui.theme
 
+import androidx.compose.runtime.Composable
+import java.lang.reflect.Modifier
+
+@Composable
+fun FavoriteButton(
+    isFavorite: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+)
